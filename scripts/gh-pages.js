3f32ef65cc2eef95ac9a7b490e8964174ffa63e5
moskowitza <moskowitzaaron@mac.com>
2020-12-17 15:55:42 -0500

    deploy script

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,7 +1,7 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-    '__sapper__/export/<your-app-name>',
+    '__sapper__/export/moskowitza-blog',
     {
         branch: 'master',
         repo: 'https://github.com/moskowitza/moskowitza-blog.git',
